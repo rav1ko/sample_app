d740c6cb5dc7803589b2b5335a5dc734f3998869
rav1ko <jaredrickert52@gmail.com>
2014-07-28 13:51:43 -0500

    finished sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,25 @@
 class User < ActiveRecord::Base
   has_secure_password
   before_save { self.email = email.downcase }
+  before_create :create_remember_token
   validates :name, presence: true, length: { maximum: 50 }
   #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i #origin regex
   VALID_EMAIL_REGEX = /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
   validates :email, presence: true, uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
   validates :password, length: { minimum: 6 }
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::MD5::hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.digest(User.new_remember_token)
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   it { should be_valid }
 
@@ -22,7 +23,6 @@ describe User do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
-
 
   describe "email is not present" do
     before { @user.email = " " }
@@ -83,6 +83,11 @@ describe User do
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
+  end
+
+  describe " remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 
   describe "return value of authenticate method" do
